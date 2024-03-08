7a5c7bb7aed988a4b63d6afd94d798f3277ce422
Dmitry Bragin <dnbragin@outlook.com>
2024-03-08 18:16:01 +0300

    init project

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Dmitrii Bragin on 08.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
