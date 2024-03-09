5005fd500dfb8eac6f480e604a3c860c7c0ddff4
Dmitry Bragin <dnbragin@outlook.com>
2024-03-09 14:24:40 +0300

    add base colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  AppColors.swift
+//  FoodDeliveryApp
+//
+//  Created by Dmitrii Bragin on 09.03.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accent = UIColor.hex(0xD35400)
+    static let grey = UIColor.hex(0xECF0F1)
+    static let black = UIColor.hex(0x000000)
+    static let white = UIColor.hex(0xE5E5E5)
+}
